7eb4c9224430eb1e225aea0155ebabe4a87ef6d8
Thomas Hug <thomas.hug@staxe.io>
2022-02-02 16:00:45 +0100

    Cleanup

--- contracts/StaxeDAOToken.sol
+++ contracts/StaxeDAOToken.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "@openzeppelin/contracts/utils/structs/BitMaps.sol";
 
 contract StaxeDAOToken is ERC20, ERC20Permit, Ownable {
   mapping(address => uint256) public claimed;
