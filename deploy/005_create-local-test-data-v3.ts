cf932dcc7eca383f1f92911e369e0f2e7c18b2d0
Thomas Hug <thomas.hug@staxe.io>
2022-08-25 16:11:57 +0200

    DEV-57 Add test data for v3 local dev

---
+++ deploy/005_create-local-test-data-v3.ts
@@ -0,0 +1,29 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { harness, newProduction } from '../test/utils/harness';
+
+const main: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const chainId = +(await hre.getChainId());
+  if (chainId !== 1337) {
+    console.error('Not a dev env, exiting');
+    return;
+  }
+
+  const { factory, productions, approver, organizer } = await harness();
+
+  const production = newProduction(100, 1n * 10n ** 18n, [
+    { minTokensRequired: 1, total: 10 },
+    { minTokensRequired: 5, total: 5 },
+    { minTokensRequired: 10, total: 1 },
+  ]);
+
+  const tx = await factory.connect(organizer).createProduction(production);
+  const confirmed = await tx.wait();
+  const id = confirmed.events?.filter((val) => val.event === 'ProductionCreated').map((val) => val.args?.id)[0];
+  await productions.connect(approver).approve(id);
+
+  console.log('Production created:', id.toNumber(), production);
+};
+
+module.exports = main;
+module.exports.tags = ['ui'];
