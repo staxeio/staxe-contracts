537e1ee0cef248a008b03cb9391238b325b3f59c
Thomas Hug <thomas.hug@staxe.io>
2022-08-30 10:26:35 +0200

    DEV-57 buy tokens for investor

--- deploy/005_create-local-test-data-v3.ts
+++ deploy/005_create-local-test-data-v3.ts
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { harness, newProduction } from '../test/utils/harness';
+import { getQuote } from '../test/utils/uniswap';
 
 const main: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const chainId = +(await hre.getChainId());
@@ -9,7 +10,7 @@ const main: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     return;
   }
 
-  const { factory, productions, approver, organizer } = await harness();
+  const { factory, productions, approver, investor1, organizer } = await harness();
 
   const production = newProduction(100, 1n * 10n ** 6n, [
     { minTokensRequired: 1, total: 10 },
@@ -23,6 +24,14 @@ const main: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   await productions.connect(approver).approve(id);
 
   console.log('Production created:', id.toNumber(), production);
+  const tokensToBuy = 10;
+  const price = await productions.connect(investor1).getTokenPrice(id, tokensToBuy);
+  const swapPrice = await getQuote(price[0], price[1].toBigInt(), 1337);
+  const tx2 = await productions
+    .connect(investor1)
+    .buyTokensWithCurrency(id, investor1.address, tokensToBuy, 3, { value: swapPrice });
+  await tx2.wait();
+  console.log(`Investor ${investor1.address} bought ${tokensToBuy} tokens`);
 };
 
 module.exports = main;
