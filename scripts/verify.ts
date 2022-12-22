61c1c4e46c3b6b6b44b67d86d8347f4d294abcdf
Thomas Hug <thomas.hug@staxe.io>
2022-12-22 15:32:39 +0100

    DEV-108 Polygon deployment

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -1,27 +1,30 @@
-import { run, ethers, getChainId } from 'hardhat';
+import { run, getChainId } from 'hardhat';
 
-import deployments from '../deploy/deployments.json';
+import deployments from '../deployments/deployments-v3.json';
 
 async function main() {
   const chainId = +(await getChainId());
   const contract = deployments.contracts.filter((contract) => contract.chainId === chainId)[0];
-
+  /*
+  console.log('Verifying token contract');
   // Token Contract
   await run('verify:verify', {
     address: contract.token,
     constructorArguments: [],
   });
 
+  console.log('Verifying members contract');
   // Members Contract
   await run('verify:verify', {
     address: contract.members,
     constructorArguments: [],
-  });
+  });*/
 
+  console.log('Verifying productions contract');
   // Productions Contract
   await run('verify:verify', {
     address: contract.productions,
-    constructorArguments: [contract.token, contract.factory, contract.members, contract.treasury],
+    constructorArguments: [contract.forwarder],
   });
 }
 
